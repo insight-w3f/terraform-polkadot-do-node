af644c54e8098b4dfecc07837d6fdeb763ff1827
Richard Mah <dick.mah@gmail.com>
2020-04-07 01:03:17 -0400

    configure default tests

--- examples/defaults/main.tf
+++ examples/defaults/main.tf
@@ -1,4 +1,17 @@
+provider "digitalocean" {}
+
+variable "public_key_path" {}
+
+module "network" {
+  source = "github.com/insight-w3f/terraform-polkadot-do-network.git?ref=master"
+}
 
 module "defaults" {
-  source = "../.."
+  source            = "../.."
+  create            = true
+  create_eip        = true
+  node_name         = "cci-test"
+  security_group_id = module.network.sentry_security_group_id[0]
+  public_key_path   = var.public_key_path
+  eph_volume_size   = "200"
 }
